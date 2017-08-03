ac094874802774f8c9afcd4bc54eec54b6ef63e9
Keith Showalter <kshowalter@fsec.ucf.edu>
2017-08-03 09:49:02 -0400

    Adding old code.

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Markdown Loader
 
 Converts markdown into specDOM.
+
+
+## Javascript
+
+Anything inside "${" and "}" will be evaluated as javascript. (see [Template literals](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals))
+
+Anything inside "<<" and ">>" will be added as parameters of the element.
+
+Example:
+
+    # Title <<class="title">>
+
+    <h1 class="title"></h1>
--- index.js
+++ index.js
@@ -2,10 +2,46 @@
 
 /**
  * .
- * @exports 
+ * @exports
  */
 module.exports = function(markdown_text){
+  var children = [];
 
+  var lines = markdown_text.split('/n');
+
+  console.log(lines[0]);
+
+  var regexIndent = /^\s+/;
+  var regexMark = /^\#+|^\*|^\-{3}/;
+
+  var lastIndent = 0;
+
+  lines.forEach(function(line){
+    var indent = line.match(regexIndent);
+    line = line.trim();
+
+    var mark = line.match(regexMark);
+
+    var lineText;
+    if( mark ){
+      lineText = line.slice(regexMark.length).trim();
+    } else {
+      lineText = line;
+    }
+
+    if( mark[0] === '#' ){ //heading
+      var hlevel = line.match(/^#./)[0].length;
+      children.push({
+        tag: 'h'+hlevel,
+        text: line
+      });
+    } else if( mark === '*' ){
+      
+    }
+
+
+
+  });
 
   return false;
 };
